2b9f601c4645d0568d0a0aa41a645a627807a7b3
DivyaMishra6 <divyamishra599@gmail.com>
2023-06-07 01:15:46 +0530

    Greeting

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Employee_Wages_Day1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage Problem");
+
+            EmployeeWage employee = new EmployeeWage();
+            employee.EmployeeAttendence();
+
+
+        }
+    }
+
+
+}
