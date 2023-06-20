2b28042368a7197f6d416944f931c7688c4b0df9
DivyaMishra6 <divyamishra599@gmail.com>
2023-06-20 19:53:19 +0530

    [Divya]: Calculate wages for Multiple company

--- Program.cs
+++ Program.cs
@@ -6,9 +6,25 @@
         {
             Console.WriteLine("Welcome to employee wage Problem");
 
+            // Compute Employee wages for multiple Company
+
+            Console.WriteLine("Enter the name of the company Dmart or Relince:");
+            string company = Console.ReadLine();
+
             EmployeeWage wage = new EmployeeWage();
-            wage.ComputeEmpWage();
-               
+            if (company == "Dmart")
+            {
+                wage.ComputeEmpWage(company, 20, 10, 90);
+            }
+            else if (company == "Relince") 
+            {
+                wage.ComputeEmpWage(company, 20, 10, 90);
+            }
+            else 
+            {
+                Console.WriteLine("Please enter correct company!!"); 
+            }
+
 
         }
     }
